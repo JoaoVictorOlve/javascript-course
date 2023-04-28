7808b73ba66c1b24aa4a18c627295f2270072132
João Victor <jvoliveira2005@gmail.com>
2023-04-27 21:20:22 -0300

    Section 5 and Lesson 40
    Parei na hora 6:27:33

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -2,12 +2,20 @@ const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 
-let myLeads = []
+myLeads = []
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    renderLeads()
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
 })
 
 function renderLeads(){
