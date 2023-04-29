f7d86c4a89abdc3ed2a576b445f215594fef6f66
João Victor <jvoliveira2005@gmail.com>
2023-04-28 21:31:23 -0300

    Section 6 and Lesson 1
    Parei na hora 7:21:33

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,28 +1,45 @@
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const tabBtn = document.querySelector("#tab-btn")
+const deleteBtn = document.querySelector("#delete-btn")
 const ulEl = document.querySelector("#ul-el")
 
 myLeads = []
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads(){
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+function render(leads){
     let listItems = ""
 
-for (let i = 0; i < myLeads.length; i++){
-    listItems += `<li><a target='_blank' href='${myLeads[i]}' > ${myLeads[i]} </a></li>`
+    for (let i = 0; i < leads.length; i++){
+        listItems += `<li><a target='_blank' href='${leads[i]}' > ${leads[i]} </a></li>`
     }
 
     ulEl.innerHTML = listItems
